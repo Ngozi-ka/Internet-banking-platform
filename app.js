2e5758882c3c8ce9df8565e77b9e0dfcbb5023cf
Ngozi-ka <nwekengozika5@gmail.com>
2025-11-11 07:52:56 +1000

    update

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const account1 = {
   owner: "Jane Nnadi",
   movements: [200, 450, -400, 3000, -650, -130, 70, 5000],
   savings: [300, 400, 210, 846, 567],
-  savingsacc: 33335567,
-  spendingacc: 33334567,
+  savingsacc: 7482091563,
+  spendingacc: 5823710496,
   interestRate: 1.2, // %
   pin: 1111,
 };
@@ -12,8 +12,8 @@ const account2 = {
   owner: "Blessing Nweke",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   savings: [300, 400, 210, 846, 567],
-  savingsacc: 33335567,
-  spendingacc: 33334567,
+  savingsacc: 9135684270,
+  spendingacc: 4609871532,
   interestRate: 1.5,
   pin: 2222,
 };
@@ -22,8 +22,8 @@ const account3 = {
   owner: "Favour Onyinye Madu",
   movements: [200, -200, 340, -300, -20, 50, 400, -460, 500, -200],
   savings: [300, 400, 210, 846, 567],
-  savingsacc: 33335567,
-  spendingacc: 33334567,
+  savingsacc: 8357204918,
+  spendingacc: 9081726354,
   interestRate: 0.7,
   pin: 3333,
 };
@@ -32,8 +32,8 @@ const account4 = {
   owner: "Gift Nwokeafor",
   movements: [430, 1000, 700, 50, 90],
   savings: [300, 400, 210, 846, 567],
-  savingsacc: 33335567,
-  spendingacc: 33334567,
+  savingsacc: 4829103756,
+  spendingacc: 7612345980,
   interestRate: 1,
   pin: 4444,
 };
@@ -158,13 +158,24 @@ const summary = function (currentAccount) {
   interest();
 };
 
-const spendno = function (accounts) {
-  const save = currentAccount.savingsacc.toString().slice(-4);
-  const spend = currentAccount.spendingacc.toString().slice(-4);
+// const spendno = function (accounts) {
+//   const save = currentAccount.savingsacc.toString().slice(-4);
+//   const spend = currentAccount.spendingacc.toString().slice(-4);
 
-  saveNo.textContent = `****${save}`;
-  spendNo.textContent = `****${spend}`;
+//   saveNo.textContent = `****${save}`;
+//   spendNo.textContent = `****${spend}`;
+// };
+
+const spendno = function (currentAccount) {
+  const save = currentAccount.savingsacc;
+  const spend = currentAccount.spendingacc;
+
+  saveNo.textContent = `${save}`;
+  spendNo.textContent = `${spend}`;
 };
+
+
+
 
 loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -199,6 +210,5 @@ loginForm.addEventListener("submit", function (e) {
     });
   } else {
     errorMsg.textContent = "Invalid username or pin";
-    return;
   }
 });
