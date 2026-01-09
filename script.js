b47082db3ad3a6fe6a77cbe4c4848a7a16d4a048
Ngozi-ka <nwekengozika5@gmail.com>
2026-01-09 12:05:35 +0100

    new features

--- script.js
+++ script.js
@@ -8,8 +8,26 @@ const account1 = {
   spendingacc: 5823710496,
   interestRate: 1.2, // %
   pin: 1111,
-  transWith: ["School Bookshop", "David Musa", "Netflix", "Access Bank", "Electric Company", "Food Vendor", "Jane Smith", "Water Utility"],
-  transFor:["Textbooks", "Money sent", "Subscription payment", "Cash deposit", "Electricity bill", "Lunch purchase", "Refund received", "Water bill"]
+  transWith: [
+    "School Bookshop",
+    "David Musa",
+    "Netflix",
+    "Access Bank",
+    "Electric Company",
+    "Food Vendor",
+    "Jane Smith",
+    "Water Utility",
+  ],
+  transFor: [
+    "Textbooks",
+    "Money sent",
+    "Subscription payment",
+    "Cash deposit",
+    "Electricity bill",
+    "Lunch purchase",
+    "Refund received",
+    "Water bill",
+  ],
 };
 
 const account2 = {
@@ -20,8 +38,26 @@ const account2 = {
   spendingacc: 4609871532,
   interestRate: 1.5,
   pin: 2222,
-  transWith: ["Supermart", "Alex Johnson", "Bolt", "University Bursary", "Amazon", "Fuel Station", "Sarah Williams", "Church"],
-  transFor:["Groceries", "Transfer sent", "Transport fare", "School fees refund", "Online shopping", "Fuel purchase", "Gift received", "Donation"]
+  transWith: [
+    "Supermart",
+    "Alex Johnson",
+    "Bolt",
+    "University Bursary",
+    "Amazon",
+    "Fuel Station",
+    "Sarah Williams",
+    "Church",
+  ],
+  transFor: [
+    "Groceries",
+    "Transfer sent",
+    "Transport fare",
+    "School fees refund",
+    "Online shopping",
+    "Fuel purchase",
+    "Gift received",
+    "Donation",
+  ],
 };
 
 const account3 = {
@@ -32,8 +68,30 @@ const account3 = {
   spendingacc: 9081726354,
   interestRate: 0.7,
   pin: 3333,
-  transWith: ["Company Payroll", "Apple Store", "Landlord", "Cinema House", "Freelance Client", "Gym Center", "Internet Provider", "Mother" ,"Office HR", "Pharmacy Plus",],
-  transFor: ["Monthly salary", "Phone accessory", "House rent", "Movie ticket", "Project payment", "Membership fee", "Internet subscription", "Allowance received", "Salary payment", "Medicine purchase"]
+  transWith: [
+    "Company Payroll",
+    "Apple Store",
+    "Landlord",
+    "Cinema House",
+    "Freelance Client",
+    "Gym Center",
+    "Internet Provider",
+    "Mother",
+    "Office HR",
+    "Pharmacy Plus",
+  ],
+  transFor: [
+    "Monthly salary",
+    "Phone accessory",
+    "House rent",
+    "Movie ticket",
+    "Project payment",
+    "Membership fee",
+    "Internet subscription",
+    "Allowance received",
+    "Salary payment",
+    "Medicine purchase",
+  ],
 };
 
 const account4 = {
@@ -44,28 +102,63 @@ const account4 = {
   spendingacc: 7612345980,
   interestRate: 1,
   pin: 4444,
-  transWith: ["Airpeace", "Eze Maryann", "Spar Mall", "UBA Bank", "John Okafor", "MTN"],
-  transFor: ["Flight ticket", "Personal transfer", "Groceries", "Account funding", "Loan repayment", "Airtime purchase"]
+  transWith: [
+    "Airpeace",
+    "Eze Maryann",
+    "Spar Mall",
+    "UBA Bank",
+    "John Okafor",
+    "MTN",
+  ],
+  transFor: [
+    "Flight ticket",
+    "Personal transfer",
+    "Groceries",
+    "Account funding",
+    "Loan repayment",
+    "Airtime purchase",
+  ],
 };
-
 
 const accounts = [account1, account2, account3, account4];
 
-
 const bill1 = {
-    name: "Transportation",
-    account: 1234567890,
-    amount: 700
-  };
+  name: "Transportation",
+  account: 1234567890,
+  amount: 450,
+};
 
- const bill2 = {
-    name: "Electricity",
-    account: 1234567890,
-    amount: 900
-  };
+const bill2 = {
+  name: "Electricity",
+  account: 2345678901,
+  amount: 1400,
+};
 
-const bills = [bill1, bill2];
+const bill3 = {
+  name: "School",
+  account: 3456789022,
+  amount: 2650,
+};
 
+const bill4 = {
+  name: "Water",
+  account: 4567890233,
+  amount: 900,
+};
+
+const bill5 = {
+  name: "Internet",
+  account: 5678902344,
+  amount: 1700,
+};
+
+const bill6 = {
+  name: "Faith",
+  account: 6789023455,
+  amount: "",
+};
+
+const bills = [bill1, bill2, bill3, bill4, bill5, bill6];
 
 const greeting = document.querySelector(".greeting");
 const transactions = document.querySelector(".transactions");
@@ -111,17 +204,32 @@ const mainTransferDetails = document.querySelector(".mainTransferDetails");
 const mainTransferAmount = document.querySelector("#mainTransferAmount");
 const fromTo = document.querySelector(".from-to");
 const transferReason = document.getElementById("transferReason");
-const mainDepositForm  = document.getElementById("mainDepositForm");
-const depositAccount  = document.getElementById("depositAccount");
-const depositAmount  = document.getElementById("depositAmount");
-const payBills  = document.getElementById("payBills");
-const billType  = document.getElementById("bill-type");
-const billAmount  = document.getElementById("bill-amount");
-const billAccount  = document.getElementById("bill-account");
+const mainDepositForm = document.getElementById("mainDepositForm");
+const depositAccount = document.getElementById("depositAccount");
+const depositAmount = document.getElementById("depositAmount");
+const payBills = document.getElementById("payBills");
+const billType = document.getElementById("bill-type");
+const billAmount = document.getElementById("bill-amount");
+const billAccount = document.getElementById("bill-account");
+const transferForm = document.getElementById("transferForm");
+const transferAccount = document.getElementById("transferAccount");
+const transferDetails = document.getElementById("transferDetails");
+const transferPurpose = document.getElementById("transferPurpose");
+const transferAmount = document.getElementById("transferAmount");
+const billTypes = document.getElementById("bill-types");
+const billAmountt = document.getElementById("billAmount");
+const billAccountt = document.getElementById("billAccount");
+const billPay = document.getElementById("billPay");
+const depositt = document.getElementById("depositt");
+const depositAccountt = document.getElementById("deposit-account");
+const depositAmountt = document.getElementById("deposit-amount");
+const infoCards = document.getElementById("infoCards");
+const forCards = document.getElementById("forCards");
+const card = document.getElementById("card");
 
 //ALL BALANCES
 const updateUI = function (acc) {
-  eachTransaction(acc)
+  eachTransaction(acc);
   mainBalance(acc);
   summary(acc);
   spendno(acc);
@@ -136,7 +244,6 @@ const username = accounts.forEach(function (acc) {
     .join("");
 });
 
-
 //SET THE TIME OF THE DAY TO ENABLE APPROPRATE GREETING
 const hour = new Date().getHours();
 
@@ -149,8 +256,8 @@ const eachTransaction = function (acc) {
   transactions.innerHTML = "";
   acc.movements.forEach(function (movement, i) {
     const deposit = movement > 0 ? "deposit" : "withdrawal";
-   const transF = acc.transFor[i];
-   const transT = acc.transWith[i];
+    const transF = acc.transFor[i];
+    const transT = acc.transWith[i];
 
     const html = `<div class="transaction">
               <div>
@@ -173,23 +280,23 @@ const mainBalance = function (acc) {
   //   return acc + curr;
   // }, 0);
 
-  accounts.forEach(function(acc){
-     acc.spendingBalance = acc.movements.reduce(function (acc, curr) {
-    return acc + curr;
+  accounts.forEach(function (acc) {
+    acc.spendingBalance = acc.movements.reduce(function (acc, curr) {
+      return acc + curr;
     }, 0);
-  })
-  
-checkBalance.textContent = `$${acc.spendingBalance}`;
+  });
 
-  accounts.forEach(function(acc){
-  acc.SavingsBalance = acc.savings.reduce(function (acc, curr) {
-    return acc + curr;
-  }, 0);
-  })
+  checkBalance.textContent = `$${acc.spendingBalance}`;
 
-saveBalance.textContent = `$${currentAccount.SavingsBalance}`;
+  accounts.forEach(function (acc) {
+    acc.SavingsBalance = acc.savings.reduce(function (acc, curr) {
+      return acc + curr;
+    }, 0);
+  });
 
-const balance = function () {
+  saveBalance.textContent = `$${currentAccount.SavingsBalance}`;
+
+  const balance = function () {
     const one = [...currentAccount.movements, ...currentAccount.savings];
     const main = one.reduce(function (acc, curr) {
       return acc + curr;
@@ -198,9 +305,7 @@ const balance = function () {
   };
 
   balance();
-   
-}
-  
+};
 
 //CALCULATING OTHER BALANCES
 const summary = function (acc) {
@@ -218,7 +323,7 @@ const summary = function (acc) {
 
   const allInterest = acc.movements
     .filter((movement) => movement > 0)
-    .map((movement) => (movement * 1.5) / 100)
+    .map((movement) => (movement * acc.interestRate) / 100)
     .filter((movement) => movement > 1)
     .reduce((acc, curr) => acc + curr, 0);
 
@@ -245,7 +350,7 @@ loginBtn.addEventListener("click", function (e) {
   currentAccount = accounts.find(function (move) {
     return move.username === accountNo.value;
   });
-if (currentAccount && Number(accountPin.value) === currentAccount.pin) {
+  if (currentAccount && Number(accountPin.value) === currentAccount.pin) {
     // formContainer.classList.add("fade-out");
     formContainer.classList.remove("fade-in");
     formContainer.classList.remove("fade-out");
@@ -278,10 +383,7 @@ if (currentAccount && Number(accountPin.value) === currentAccount.pin) {
   } else {
     errorMsg.textContent = "Invalid username or pin";
   }
-
-  console.log(accounts)
 });
-
 
 //FOR LOGGING OUT
 logoutBtn.addEventListener("click", function (e) {
@@ -306,7 +408,6 @@ close.forEach(function (closebtn) {
     main.style.filter = "blur(0)";
   });
 });
-
 
 //OPENING TRANSFER, DEPOSIT, BORROW AND BILL FORMS FOR SMALL SCREEN
 transfer.addEventListener("click", function () {
@@ -343,8 +444,6 @@ activeFour.addEventListener("click", function () {
   asideForms.prepend(sendLoan);
 });
 
-
-
 //IMplementing transfers
 // DISPLAYING THE NAME OF THE TRANSFER ACCOUNT OWNER
 transferMain.addEventListener("input", function () {
@@ -365,10 +464,10 @@ mainTransferForm.addEventListener("click", function (e) {
     return acc.spendingacc === Number(transferMain.value);
   });
 
-const amount = Number(mainTransferAmount.value.trim());
- const tReason = transferReason.value;
+  const amount = Number(mainTransferAmount.value.trim());
+  const tReason = transferReason.value;
 
- if (
+  if (
     transferToAccount &&
     transferToAccount?.spendingacc !== currentAccount.spendingacc &&
     amount > 0 &&
@@ -388,82 +487,219 @@ const amount = Number(mainTransferAmount.value.trim());
     transferReason.value = "";
     sendOne.style.display = "none";
     main.style.filter = "blur(0)";
-    console.log("Did it again");
+    card.innerHTML = "Transfer Successful";
+    infoCards.style.display = "block";
   } else {
-    alert("Something is not right; rectify and try again later.")
-  
+    card.innerHTML = "Something is not right; rectify and try again.";
+    infoCards.style.display = "block";
   }
-})
-
-// 9081726354
-// 7612345980
+});
 
 //IMPLEMENTING DEPOSIT
-mainDepositForm.addEventListener("click", function(e){
+mainDepositForm.addEventListener("click", function (e) {
   e.preventDefault();
 
-
-    const depoAmount = Number(depositAmount.value);
-  if(Number(depositAccount.value) === currentAccount.savingsacc && depoAmount < currentAccount.spendingBalance ){
-    currentAccount.savings.push(depoAmount)
+  const depoAmount = Number(depositAmount.value);
+  if (
+    Number(depositAccount.value) === currentAccount.savingsacc &&
+    depoAmount < currentAccount.spendingBalance
+  ) {
+    currentAccount.savings.push(depoAmount);
     currentAccount.movements.push(-depoAmount);
     currentAccount.transWith.push("From spending");
-    currentAccount.transFor.push("For savings")
+    currentAccount.transFor.push("For savings");
     updateUI(currentAccount);
     depositAccount.value = "";
     depositAmount.value = "";
     sendThree.style.display = "none";
-     main.style.filter = "blur(0)";
-     alert("Deposit from spending to savings, SUCCESSFUL");
-  }else if (Number(depositAccount.value) === currentAccount.spendingacc && depoAmount < currentAccount.SavingsBalance ){
-    currentAccount.savings.push(-depoAmount)
+    main.style.filter = "blur(0)";
+    card.innerHTML = "Deposit from spending to savings, SUCCESSFUL";
+    infoCards.style.display = "block";
+  } else if (
+    Number(depositAccount.value) === currentAccount.spendingacc &&
+    depoAmount < currentAccount.SavingsBalance
+  ) {
+    currentAccount.savings.push(-depoAmount);
     currentAccount.movements.push(depoAmount);
     currentAccount.transWith.push("From savings");
-    currentAccount.transFor.push("For spending")
+    currentAccount.transFor.push("For spending");
     updateUI(currentAccount);
     depositAccount.value = "";
     depositAmount.value = "";
     sendThree.style.display = "none";
-     main.style.filter = "blur(0)";
-     alert("Deposit from savings to spending, SUCCESSFUL");
-  }else {
-    alert("Something is not right; rectify and try again later.")
-  
+    main.style.filter = "blur(0)";
+    card.innerHTML = "Deposit from savings to spending, SUCCESSFUL";
+    infoCards.style.display = "block";
+  } else {
+    card.innerHTML = "Something is not right; rectify and try again.";
+    infoCards.style.display = "block";
   }
-})
+});
 
+//IMPLEMENTING BILL/PAY
 
-//IMPLEMENTING BILL
-
-billType.addEventListener("input", function(e){
+billType.addEventListener("input", function (e) {
   e.preventDefault();
 
-  const payBill = bills.find(function(bill){
-  return bill.name === billType.value;
-})
+  const payBill = bills.find(function (bill) {
+    return bill.name === billType.value;
+  });
 
-if(payBill){
-  billAccount.value = payBill.account;
-  billAmount.value = payBill.amount;
-}
-})
+  if (payBill) {
+    billAccount.value = payBill.account;
+    billAmount.value = payBill.amount;
+  }
+});
 
-payBills.addEventListener("click", function(e){
+payBills.addEventListener("click", function (e) {
   e.preventDefault();
 
-  if(Number(billAmount.value) < currentAccount.spendingBalance){
+  if (Number(billAmount.value) < currentAccount.spendingBalance) {
     currentAccount.movements.push(-billAmount.value);
-currentAccount.transWith.push("Paid bill");
-currentAccount.transFor.push(billType.value)
-updateUI(currentAccount)
-billType.value = "";
- billAccount.value = "";
-  billAmount.value = "";
-  sendTwo.style.display = "none";
-   main.style.filter = "blur(0)";
-  }else{
-     alert("Something is not right; rectify and try again later.")
+    currentAccount.transWith.push("Paid bill");
+    currentAccount.transFor.push(billType.value);
+    updateUI(currentAccount);
+    billType.value = "";
+    billAccount.value = "";
+    billAmount.value = "";
+    sendTwo.style.display = "none";
+    main.style.filter = "blur(0)";
+    card.innerHTML = "Payment Successful";
+    infoCards.style.display = "block";
+  } else {
+    card.innerHTML = "Something is not right; rectify and try again.";
+    infoCards.style.display = "block";
   }
+});
 
+//IMPLEMENTING TRANSFER FOR LARGER SCREENS
 
-})
+// DISPLAYING THE NAME OF THE TRANSFER ACCOUNT OWNER
+transferAccount.addEventListener("input", function () {
+  const accName = accounts.find(function (acc) {
+    return acc.spendingacc === Number(transferAccount.value);
+  });
+  if (accName) {
+    transferDetails.textContent = accName.owner;
+  } else {
+    transferDetails.textContent = "Incorrect account number";
+  }
+});
+
+transferForm.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  transferToAccount = accounts.find(function (acc) {
+    return acc.spendingacc === Number(transferAccount.value);
+  });
+
+  const amount = Number(transferAmount.value.trim());
+  const tReason = transferPurpose.value;
+
+  if (
+    transferToAccount &&
+    transferToAccount?.spendingacc !== currentAccount.spendingacc &&
+    amount > 0 &&
+    currentAccount.spendingBalance >= amount
+  ) {
+    currentAccount.movements.push(-amount);
+    transferToAccount.movements.push(amount);
+    currentAccount.transWith.push(transferToAccount.owner);
+    transferToAccount.transWith.push(currentAccount.owner);
+    currentAccount.transFor.push(tReason);
+    transferToAccount.transFor.push(tReason);
+    updateUI(currentAccount);
+    // updateUI(transferToAccount)
+    transferAccount.value = "";
+    transferAmount.value = "";
+    transferDetails.textContent = "";
+    transferPurpose.value = "";
+    card.innerHTML = `Transfer successful`;
+    infoCards.style.display = "block";
+    console.log("Did it again");
+  } else {
+    card.innerHTML = "Something is not right; rectify and try again.";
+    infoCards.style.display = "block";
+  }
+});
+
+//IMPLEMENTING BILL/PAY FOR LARGE SCREENS
+
+billTypes.addEventListener("input", function (e) {
+  e.preventDefault();
+
+  const payBill = bills.find(function (bill) {
+    return bill.name === billTypes.value;
+  });
+
+  if (payBill) {
+    billAccountt.value = payBill.account;
+    billAmountt.value = payBill.amount;
+  }
+});
+
+billPay.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (Number(billAmountt.value) < currentAccount.spendingBalance) {
+    currentAccount.movements.push(-billAmountt.value);
+    currentAccount.transWith.push("Paid bill");
+    currentAccount.transFor.push(billTypes.value);
+    updateUI(currentAccount);
+    billTypes.value = "";
+    billAccountt.value = "";
+    billAmountt.value = "";
+    sendTwo.style.display = "none";
+    main.style.filter = "blur(0)";
+  } else {
+    card.innerHTML = "Something is not right; rectify and try again.";
+    infoCards.style.display = "block";
+  }
+});
+
+//IMPLEMENTING DEPOSIT FOR LARGE SCREENS
+depositt.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const depoAmount = Number(depositAmountt.value);
+  if (
+    Number(depositAccountt.value) === currentAccount.savingsacc &&
+    depoAmount < currentAccount.spendingBalance
+  ) {
+    currentAccount.savings.push(depoAmount);
+    currentAccount.movements.push(-depoAmount);
+    currentAccount.transWith.push("From spending");
+    currentAccount.transFor.push("For savings");
+    updateUI(currentAccount);
+    depositAccountts.value = "";
+    depositAmountt.value = "";
+    sendThree.style.display = "none";
+    main.style.filter = "blur(0)";
+    card.innerHTML = "Deposit from spending to savings is SUCCESSFUL";
+    infoCards.style.display = "block";
+  } else if (
+    Number(depositAccountt.value) === currentAccount.spendingacc &&
+    depoAmount < currentAccount.SavingsBalance
+  ) {
+    currentAccount.savings.push(-depoAmount);
+    currentAccount.movements.push(depoAmount);
+    currentAccount.transWith.push("From savings");
+    currentAccount.transFor.push("For spending");
+    updateUI(currentAccount);
+    depositAccountt.value = "";
+    depositAmountt.value = "";
+    sendThree.style.display = "none";
+    main.style.filter = "blur(0)";
+    card.innerHTML = "Deposit from savings to spending is SUCCESSFUL";
+    infoCards.style.display = "block";
+  } else {
+    card.innerHTML = "Something is not right; rectify and try again later.";
+    infoCards.style.display = "block";
+  }
+});
+
+forCards.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  infoCards.style.display = "none";
+});
